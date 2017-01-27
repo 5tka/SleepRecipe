7f5c839223468e30fcfa44e91821004e190d816b
5tka <ferol2010@yandex.ru>
2017-01-27 11:54:24 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,45 @@
+console.log('hello from script.js');
+$( document ).ready(function() { // начало document.ready
+
+
+    $('.mnu-item').click(function(ev){
+        ev = event.preventDefault();
+        $(this)
+            .toggleClass('mnu-item--active')
+            .find('.sub-list').stop(true).slideToggle('fast');
+    });
+
+
+
+    $('.bestsellers-items').bxSlider({
+        slideWidth: 270,
+        // infiniteLoop:!0,
+        hideControlOnEnd:!0,
+        pager:!1,
+        nextText:"",
+        prevText:"",
+        minSlides:1,
+        maxSlides:4,
+        moveSlides:1
+    });
+    $('.brands-slider').bxSlider({
+        slideWidth: 80,
+        slideMargin: 50,
+        // infiniteLoop:!0,
+        hideControlOnEnd:!0,
+        pager:!1,
+        nextText:"",
+        prevText:"",
+        minSlides:1,
+        maxSlides: 8,
+        moveSlides:1
+    });
+
+
+
+
+
+
+
+
+});
